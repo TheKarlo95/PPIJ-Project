28e75872c471b02bbb2565d26fcc758290e98786
tinicek <tz48424@fer.hr>
2017-05-16 17:13:04 +0200

    perfume profile take 1

---
+++ app/src/main/java/hr/lordsofsmell/parfume/feature/perfumeProfile/IPerfumeProfile.java
@@ -0,0 +1,8 @@
+package hr.lordsofsmell.parfume.feature.perfumeProfile;
+
+/**
+ * Created by tea03 on 5/16/2017.
+ */
+
+public interface IPerfumeProfile {
+}
---
+++ app/src/main/java/hr/lordsofsmell/parfume/feature/perfumeProfile/presenter/PerfumeProfilePresenter.java
@@ -0,0 +1,11 @@
+package hr.lordsofsmell.parfume.feature.perfumeProfile.presenter;
+
+import hr.lordsofsmell.parfume.feature.core.presenter.Presenter;
+import hr.lordsofsmell.parfume.feature.perfumeProfile.IPerfumeProfile;
+
+/**
+ * Created by tea03 on 5/16/2017.
+ */
+
+public class PerfumeProfilePresenter extends Presenter implements IPerfumeProfile {
+}
